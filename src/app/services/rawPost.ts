1e793cc23ae4890ee7996d1e6edd12e72f11994a
Beatriz Givisiez <beatriz.irineu.givisiez@everis.com>
2021-04-06 14:41:32 -0300

    primeiro commit

---
+++ src/app/services/rawPost.ts
@@ -0,0 +1,45 @@
+export default {
+  id: 7,
+  timestamp: '2019-02-20T13:22Z',
+  author: {
+    id: 239,
+    username: 'João Figueiredo'
+  },
+  title: 'Estratégias em um novo paradigma globalizado',
+  subtitle: 'Sobre o cuidado em identificar pontos críticos na complexidade',
+  content: '<p>Caros amigos, a mobilidade dos capitais internacionais desafia a capacidade de equalização das diversas correntes de pensamento. Nunca é demais lembrar o peso e o significado destes problemas, uma vez que a necessidade de renovação processual ainda não demonstrou convincentemente que vai participar na mudança das diretrizes de desenvolvimento para o futuro. O cuidado em identificar pontos críticos na complexidade dos estudos efetuados é uma das consequências das direções preferenciais no sentido do progresso. Do mesmo modo, a adoção de políticas descentralizadoras não pode mais se dissociar dos modos de operação convencionais.</p><p>Acima de tudo, é fundamental ressaltar que o novo modelo estrutural aqui preconizado cumpre um papel essencial na formulação das novas proposições. A nível organizacional, o desenvolvimento contínuo de distintas formas de atuação oferece uma interessante oportunidade para verificação dos conhecimentos estratégicos para atingir a excelência. Por outro lado, a constante divulgação das informações exige a precisão e a definição do sistema de formação de quadros que corresponde às necessidades. É claro que a contínua expansão de nossa atividade estimula a padronização das condições inegavelmente apropriadas.</p>',
+  comments: [
+    {
+      id: 1,
+      respondsTo: null,
+      author: {
+        id: 292,
+        username: 'Joana Vasconcellos'
+      },
+      timestamp: '2019-02-20:30Z',
+      content: 'O empenho em analisar a consolidação das estruturas oferece uma interessante oportunidade para verificação do retorno esperado a longo prazo. Por outro lado, o julgamento imparcial das eventualidades facilita a criação dos modos de operação convencionais. O que temos que ter sempre em mente é que a revolução dos costumes estimula a padronização das novas proposições.'
+    },
+    {
+      id: 2,
+      respondsTo: null,
+      author: {
+        id: 294,
+        username: 'Arthur Silveira'
+      },
+      timestamp: '2019-02-17T11:23Z',
+      content: 'É importante questionar o quanto o consenso sobre a necessidade de qualificação possibilita uma melhor visão global dos índices pretendidos.'
+    },
+    {
+      id: 3,
+      respondsTo: {
+        id: 2
+      },
+      author: {
+        id: 298,
+        username: 'Clara Passos'
+      },
+      timestamp: '2019-02-23T07:48Z',
+      content: 'Ainda assim, existem dúvidas a respeito de como a execução dos pontos do programa representa uma abertura para a melhoria da gestão inovadora da qual fazemos parte. Não obstante, a crescente influência da mídia aponta para a melhoria das direções preferenciais no sentido do progresso.'
+    }
+  ]
+};
